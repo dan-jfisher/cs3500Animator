ccfb105860c919ea3c448f437de361d85632286f
Kevin Leiser <Kevin@Kevins-MacBook-Pro.local>
2018-06-15 01:17:44 -0400

    new text file covering some things to test for

--- src/cs3500/animator/controller/ControllerGUI.java
+++ src/cs3500/animator/controller/ControllerGUI.java
@@ -1,34 +1,34 @@
 package cs3500.animator.controller;
 
 import java.awt.Shape;
+import java.util.Timer;
 
 import cs3500.animator.model.animation.IAnimatedShape;
 import cs3500.animator.model.animation.IAnimationModel;
 import cs3500.animator.view.ViewGUI;
 
 /**
- *
+ * This controller controls a view that uses a GUI. The frame rate of the GUI is controlled from
+ * here and updated shapes are passed to the view every frame.
  */
 public class ControllerGUI extends AbstractController {
 
   private ViewGUI guiView;
 
   /**
-   *
-   * @param model
-   * @param guiView
-   * @param frameRatePerSec
+   * Constructor for the GUI controller. Reads data from model and tells the view what to print.
+   * @param model model to be used to get animation information.
+   * @param guiView View to display animation in a GUI.
+   * @param frameRatePerSec frame rate at which the animation will run.
    */
   public ControllerGUI(IAnimationModel model, ViewGUI guiView, int frameRatePerSec) {
-    return;
+    this.guiView = guiView;
+    this.model = model;
+    this.frameRate = frameRatePerSec;
   }
 
   @Override
   public void run() {
-    //EMPTY
-  }
-
-  private Shape animatedShapeToAwtShape(IAnimatedShape animatedShape) {
-    return null;
+    
   }
 }
